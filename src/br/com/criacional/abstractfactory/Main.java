5f7b48189f527926e50b413b6429559054eed8c2
Wédson Gomes <40505932+wedgomes@users.noreply.github.com>
2024-09-07 13:38:55 -0300

    adiciona lógica de outros transportes como aeronaves

--- src/br/com/criacional/abstractfactory/Main.java
+++ src/br/com/criacional/abstractfactory/Main.java
@@ -1,7 +1,29 @@
 package br.com.criacional.abstractfactory;
 
-public class Main {
-    public static void main(String[] args) {
+import br.com.criacional.abstractfactory.app.Aplication;
+import br.com.criacional.abstractfactory.factories.ITransportFactory;
+import br.com.criacional.abstractfactory.factories.NineNineTransport;
+import br.com.criacional.abstractfactory.factories.UberTransport;
 
+public class Main {
+
+    public static Aplication configureAplication(){
+        Aplication app;
+        ITransportFactory factory;
+
+        String company = "uber";
+
+        if(company == "99"){
+            factory = new UberTransport();
+        }else {
+            factory = new NineNineTransport();
+        }
+        app = new Aplication(factory);
+
+        return app;
+    }
+    public static void main(String[] args) {
+        Aplication app = configureAplication();
+        app.startRoute();
     }
 }
